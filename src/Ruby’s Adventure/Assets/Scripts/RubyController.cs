b77f5287aab4ac5f5e9dc75b1647316d0602a355
Uroš Miletić <uros.miletic@ips-ag.com>
2022-11-19 04:15:39 +0100

    Add rigidbody to Ruby Add colliders to game objects and tiles

--- src/Ruby’s Adventure/Assets/Scripts/RubyController.cs
+++ src/Ruby’s Adventure/Assets/Scripts/RubyController.cs
@@ -2,19 +2,29 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    private float horizontal;
+    private Rigidbody2D rigidbody2d;
+    private float vertical;
+
     // Start is called before the first frame update
     private void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        var position = rigidbody2d.position;
         position.x += 3.0f * horizontal * Time.deltaTime;
         position.y += 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
     }
 }
