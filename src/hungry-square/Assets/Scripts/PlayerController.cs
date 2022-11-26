5e7d605f9b24cf0222434af328588d06cd2af31d
Uroš Miletić <uros.miletic@ips-ag.com>
2022-11-26 04:57:58 +0100

    Make player move

--- src/hungry-square/Assets/Scripts/PlayerController.cs
+++ src/hungry-square/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 3.0f;
+
+    // movement
+    private Rigidbody2D _rigidbody2D;
+    private float _vertical;
+    private float _horizontal;
+
+    private void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        var position = _rigidbody2D.position;
+        var move = new Vector2(_horizontal, _vertical);
+        position += move * (speed * Time.deltaTime);
+        _rigidbody2D.MovePosition(position);
     }
 }
